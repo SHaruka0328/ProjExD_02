8a3b1f58197776f60a5a8270ba3ca61777fada2c
SakuraiRin <68419969+SakuraiRin@users.noreply.github.com>
2023-04-25 14:13:07 +0900

    練習03

--- dodge_bomb_original.py
+++ dodge_bomb_original.py
@@ -9,17 +9,15 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-
     # 練習01
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0))
-
     # 練習02
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, 1600), random.randint(0, 900)
     #screen.blit(bb_img, bb_rct)
-
+    vx, vy = +1, +1
 
     tmr = 0
 
@@ -31,7 +29,9 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
+        
         pg.display.update()
         clock.tick(1000)
 
